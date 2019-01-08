bbc6673f2b1bcbe1f303486b716016aa9b01b5a2
Nuno Neto <nuno_mgl@hotmail.com>
2019-01-08 03:09:15 +0000

    added pickers

---
+++ Assets/PickerMechanic.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickerMechanic : MonoBehaviour
+{
+
+    public GameController gc;
+    public Pickup pickup;
+
+    public enum Pickup
+    {
+        Default,
+        Sprint,
+        Pistol,
+        Hook
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.transform.tag.Equals("Player"))
+        {
+            if(pickup == Pickup.Sprint)
+            {
+                gc.canSprint = true;
+            }
+            else if (pickup == Pickup.Pistol)
+            {
+                gc.canShoot = true;
+            }
+            else if (pickup == Pickup.Hook)
+            {
+                gc.canGrapple = true;
+            }
+            Destroy(gameObject);
+        }
+    }
+}
